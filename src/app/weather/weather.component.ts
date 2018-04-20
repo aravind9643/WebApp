aca335238ec55be548af8035dfb9e70b3b9bafa9
aravind9643 <aravind9643@gmail.com>
2018-04-20 20:39:17 +0530

    changed some elements in weather tab

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -8,7 +8,7 @@ import 'rxjs/add/operator/map';
   styleUrls: ['./weather.component.css']
 })
 export class WeatherComponent implements OnInit {
-  temp: number;
+  temp: any;
   name: string;
   humidity: number;
   weather: {};
@@ -17,7 +17,7 @@ export class WeatherComponent implements OnInit {
   speed: number;
   longitude;
   latitude;
-  hide;
+  city;
   constructor(private jsonp: Jsonp) {
       }
   ngOnInit() {
@@ -27,7 +27,8 @@ export class WeatherComponent implements OnInit {
       });
 
 }
-setWeather() {
+setWeatherByGPS() {
+  if (this.latitude !== undefined) {
   // tslint:disable-next-line:max-line-length
   this.jsonp.get('http://api.openweathermap.org/data/2.5/weather?lat=' + this.latitude + '&lon=' + this.longitude + '&appid=83a004a4e9841d7258c25e987aba9c88&callback=JSONP_CALLBACK')
   .subscribe(data => {
@@ -38,6 +39,24 @@ setWeather() {
     this.speed = data.json().wind.speed;
     this.pressure = data.json().main.pressure;
      });
-     this.hide = !this.hide;
+    } else {
+      alert('Please Wait');
+    }
+}
+setWeatherByCity() {
+  if (this.city !== undefined) {
+    // tslint:disable-next-line:max-line-length
+  this.jsonp.get('http://api.openweathermap.org/data/2.5/weather?q=' + this.city + '&appid=83a004a4e9841d7258c25e987aba9c88&callback=JSONP_CALLBACK')
+  .subscribe(data => {
+    this.temp = (data.json().main.temp - 273.15).toFixed(3);
+    this.name = data.json().name;
+    this.humidity = data.json().main.humidity;
+    this.weather = data.json().weather;
+    this.speed = data.json().wind.speed;
+    this.pressure = data.json().main.pressure;
+     });
+    } else {
+      alert('Enter City Name');
+    }
 }
 }
